69391345a872915b418832c326e2e48c4f9e0974
Abderezak-Lamari <abderezaklamari@gmail.com>
2025-02-01 17:02:12 +0100

    a

--- src/Welcom.jsx
+++ src/Welcom.jsx
@@ -7,7 +7,7 @@ function Welcom(){
         <>
           <Footer />
           <div className="dumbdiv">
-            <img className="fComputer" src="resources\it-admin-troubleshooting.gif" alt="Computer"></img>
+            <img className="fComputer" src=".\resources\it-admin-troubleshooting.gif" alt="Computer"></img>
             <p>--- Feel Free to look around</p>
           </div>
           <p>--- Welcome to the web ---</p>
@@ -22,7 +22,7 @@ function Welcom(){
                   <li>Currently doing something</li>
                 </ul>
                 <div className="dumbdiv">
-                  <img className="nimg" src="resources\062c7ea2fdf5b8fb6b471a129649e237_w200.gif" alt="Brain"></img>
+                  <img className="nimg" src=".\resources\062c7ea2fdf5b8fb6b471a129649e237_w200.gif" alt="Brain"></img>
                   <p>--- Thanks For Visiting My Website</p>
                 </div>
               </div>
