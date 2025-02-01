1c0c461528384356169d650a51f2dd31461dabc6
Abderezak-Lamari <abderezaklamari@gmail.com>
2025-02-01 15:48:41 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Home/',
   plugins: [react()],
   server: {
     host: true,  // Allow access from external network
